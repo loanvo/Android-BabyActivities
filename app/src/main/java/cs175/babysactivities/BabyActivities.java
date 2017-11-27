0df5e610e75a3f8860c26cfd00d5407b6cb67ea7
loan vo <loanvotx@gmail.com>
2017-11-27 14:37:03 -0800

    Implement temperature display on screen - use sensor TYPE_PRESSURE plug in Hypsometric formular to cal temp

--- app/src/main/java/cs175/babysactivities/BabyActivities.java
+++ app/src/main/java/cs175/babysactivities/BabyActivities.java
@@ -3,6 +3,10 @@ package cs175.babysactivities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
@@ -10,12 +14,22 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class BabyActivities extends AppCompatActivity {
+public class BabyActivities extends AppCompatActivity implements SensorEventListener{
 
     private TextView mTextMessage;
-    SharedPreferences sharedPreferences;
 
+    SensorManager sensorManager;
+    Sensor sensor;
+    int temperature =0;
+
+    //constant to calculate temperature references at keisan.casio.com
+    final static float CONSTANT1 = 5.257F;
+    final static float CONSTANT2 = 0.0065F;
+    final static float CONSTANT3 = 273.15F;
+
+    private TextView tempView;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -44,7 +58,43 @@ public class BabyActivities extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        tempView = (TextView) findViewById(R.id.temp_view);
+
+        sensorManager =(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
     }
 
+    @Override
+    public void onResume() {
 
+        super.onResume();
+        Sensor tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if(tempSensor != null){
+            sensorManager.registerListener(this, tempSensor, sensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            Toast.makeText(this, "Sensor not found", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_PRESSURE);{
+           float pressure;
+           float altitude;
+           float sealevel;
+            pressure = (float) event.values[0];
+            sealevel = SensorManager.PRESSURE_STANDARD_ATMOSPHERE;
+            altitude = SensorManager.getAltitude(sealevel, pressure);
+            temperature = (int) Math.round((1/(1 - Math.pow(Math.E, Math.log(pressure/sealevel)/CONSTANT1)))
+                    *(CONSTANT2*altitude) - CONSTANT2*altitude - CONSTANT3);
+            tempView.setText(Double.toString(temperature));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
